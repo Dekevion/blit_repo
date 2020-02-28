2b767293ac4ce905155603f400a5d8334957b328
Dekevion Wade <Dekevionwade@yahoo.com>
2020-02-28 10:24:22 -0600

    build here

---
+++ blit/src/NewComponent.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class NewComponent extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1>Dekevion</h1>
+            </div>
+        );
+    }
+}
+
+export default NewComponent;
